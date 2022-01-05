1a1cc6fb23410e1f0f0eb283279ce22f9d9e7e05
jale020 <mujezinovicnp@gmail.com>
2022-01-05 19:52:58 +0100

    js

---
+++ Java/app.js
@@ -0,0 +1,115 @@
+// var1 = prompt("Unesi broj 1");
+// var2 = prompt("Unesi broj 2");
+
+// console.log(parseInt);
+
+// a = prompt("Unesi broj a");
+// b = prompt("Unesi broj b");
+// console.log(a > b);
+
+// a = prompt("Unesi broj od 1 do 10");
+// console.log(a < 10 && a > 1);
+
+// a = prompt("Da li je broj paran?");
+// console.log(a % 2 === 0);
+
+// a = prompt("Unesite vase ime");
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.concat("Mujezinovic"));
+// console.log(a.indexOf("s"));
+// console.log(a.includes("w"));
+
+// str = "Apple, Banana, Kiwi";
+// part = str.slice(7, 13);
+// console.log(part);
+
+// text = "Please visit Microsoft!";
+// newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText);
+
+// a = prompt("unesite broj");
+// b = prompt("unesite broj");
+// if (a > b) {
+//   console.log("Prvi broj je veci");
+// }
+// if (b > a) {
+//   console.log("Drugi broj je veci");
+// }
+
+// a = prompt("Unesite broj od 1-10");
+// if (a < 10) {
+//   console.log("Tacno");
+// }
+// if (a > 10) {
+//   console.log("Netacno");
+// }
+
+// a = 10;
+// b = 1;
+
+// if (a > b) {
+//   console.log("Pokazi a");
+// }
+// if (b < a) {
+//   console.log("Pokazi b");
+// }
+
+// a = prompt("Unesite broj bodova");
+
+// if (a >= 50 && a <= 65) {
+//   console.log("Ocena 6");
+// } else if (a >= 65 && a <= 75) {
+//   console.log("Ocena 7");
+// } else if (a >= 75 && a <= 85) {
+//   console.log("Ocena 8");
+// } else if (a >= 85 && a <= 95) {
+//   console.log("Ocena 9");
+// } else if (a >= 95 && a <= 100);
+// console.log("Ocena 10");
+
+// a = parseInt(prompt("Unesi broj izmedju 1-7"));
+
+// switch (a) {
+//   case 1:
+//     console.log("Ponedeljak");
+//     break;
+//   case 2:
+//     console.log("Utorak");
+//     break;
+//   case 3:
+//     console.log("Sreda");
+//     break;
+//   case 4:
+//     console.log("Cetvrtak");
+//     break;
+//   case 5:
+//     console.log("Petak");
+//     break;
+//   case 6:
+//     console.log("Subota");
+//     break;
+//   case 7:
+//     console.log("Nedelja");
+//     break;
+//   default:
+//     console.log("Pogresan unos");
+// }
+
+// a = parseInt(prompt("Unesite broj od 0 - 100"));
+
+// switch (true) {
+//   case a > 0 && a < 30:
+//     console.log("Low");
+//     break;
+//   case a > 30 && a < 60:
+//     console.log("Medium");
+//     break;
+//   case a > 70 && a < 100:
+//     console.log("High");
+//     break;
+// }
+
+// for (i = 7; i > 0; i--) {
+//   console.log(i);
+// }
